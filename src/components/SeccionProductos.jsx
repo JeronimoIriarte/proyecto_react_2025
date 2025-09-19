3168f69f39567b5c67cbeb2b07618605c5dbe653
JeronimoIriarte <iriartejerinimo7@gmail.com>
2025-09-19 01:59:36 -0300

    Cambios Generales

--- src/components/SeccionProductos.jsx
+++ src/components/SeccionProductos.jsx
@@ -2,46 +2,50 @@ import styles from "../styles/SeccionProductos.module.css";
 
 export default function SeccionProductos() {
   // Datos de ejemplo de las camisetas
-    const productos = [
+  const productos = [
     {
-    id: 1,
-    nombre: "Camiseta Seleccion Argentina",
-    precio: "$5000",
-    imagen: "/images/camisetaarg.jpg"
+      id: 1,
+      nombre: "Camiseta Seleccion Argentina",
+      precio: "$5000",
+      imagen: "/images/camisetaarg.jpg",
     },
     {
-    id: 2,
-    nombre: "Camiseta Rosario Central",
-    precio: "$5200",
-    imagen: "/images/camisetacentral.jpg"
+      id: 2,
+      nombre: "Camiseta Rosario Central",
+      precio: "$5200",
+      imagen: "/images/camisetacentral.jpg",
     },
     {
-    id: 3,
-    nombre: "Camiseta Lanus",
-    precio: "$4800",
-    imagen: "/images/camisetalanus.jpg"
+      id: 3,
+      nombre: "Camiseta Lanus",
+      precio: "$4800",
+      imagen: "/images/camisetalanus.jpg",
     },
     {
-    id: 4,
-    nombre: "Camiseta Seleccion Argentina Retro",
-    precio: "$5000",
-    imagen: "/images/retroarg.jpg"
+      id: 4,
+      nombre: "Camiseta Seleccion Argentina Retro",
+      precio: "$5000",
+      imagen: "/images/retroarg.jpg",
     },
-    ];
+  ];
 
-return (
+  return (
     <section className={styles.productosSection}>
-    <h2 className={styles.titulo}>Nuestros Productos</h2>
-    <div className={styles.productosContainer}>
+      <h2 className={styles.titulo}>Ofertas Especiales</h2>
+      <div className={styles.productosContainer}>
         {productos.map((prod) => (
-        <div key={prod.id} className={styles.card}>
-            <img src={prod.imagen} alt={prod.nombre} className={styles.cardImage} />
+          <div key={prod.id} className={styles.card}>
+            <img
+              src={prod.imagen}
+              alt={prod.nombre}
+              className={styles.cardImage}
+            />
             <h3>{prod.nombre}</h3>
             <p>{prod.precio}</p>
             <button>Ver más</button>
-        </div>
+          </div>
         ))}
-    </div>
+      </div>
     </section>
-);
+  );
 }
